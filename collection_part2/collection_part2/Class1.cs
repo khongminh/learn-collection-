46f913b2d87f5233cc1051b0d0981b25485f2741
mrechcheo149162536 <mrechcheo149162536@gmail.com>
2017-10-07 16:25:43 +0700

    learn dictionary

---
+++ collection_part2/collection_part2/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace collection_List
+{
+	class NonblankList : Collection<string>
+	{
+		protected override void InsertItem(int index, string item)
+		{
+			if (string.IsNullOrWhiteSpace(item))
+				throw new ArgumentException();
+
+			base.InsertItem(index, item);
+		}
+
+		protected override void SetItem(int index, string item)
+		{
+			if (string.IsNullOrWhiteSpace(item))
+				throw new ArgumentException();
+			base.SetItem(index, item);
+		}
+	}
+
+}
--- collection_part2/collection_part2/Program.cs
+++ collection_part2/collection_part2/Program.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 
-namespace collection_Array
+namespace collection_List
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			var presidents = new LinkedList<string>();
+
+			presidents.AddLast("JFK");
+			presidents.AddLast("Lyndon B Johnson");
+			presidents.AddLast("Rechard Nixon");
+			presidents.AddLast("Jimmy Carter");
+
+
 			
+			foreach (string president in presidents)
+			{
+				Console.WriteLine(president);
+			}
+
 		}
+
+		
 	}
+
+	
+
 }
